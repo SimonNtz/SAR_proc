769ecc5f9c8688b11231a4f0cd14e5c2b8c1ae1a
Simon <simon.noetzlin@epfl.ch>
2017-07-05 16:42:06 +0200

    add

--- SAR_mapper.py
+++ SAR_mapper.py
@@ -61,8 +61,8 @@ parameters = HashMap()
 
 x = 0
 y = 4000
-width = 5356
-height = 5673
+width = 1356
+height = 1673
 
 parameters.put('copyMetadata', True)
 parameters.put('region',  "%s,%s,%s,%s" % (x, y, width, height))
