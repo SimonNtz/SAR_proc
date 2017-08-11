1c7374d080cce51aff18a6ac07fc1cc7ad02fce1
SimonNtz <simon.ntz@gmail.com>
2017-08-11 17:01:47 +0200

    making some try

--- SAR_mapper.py
+++ SAR_mapper.py
@@ -122,11 +122,11 @@ print(timestamp()+"start terrain-correction")
 # parameters.put('demName', 'SRTM 3Sec')
 # parameters.put('pixelSpacingInMeter', 10.0)
 
-params.put('demResamplingMethod', 'BILINEAR_INTERPOLATION')
-params.put('imgResamplingMethod', 'BILINEAR_INTERPOLATION')
-params.put('demName', 'ASTER 1Sec GDEM')
-params.put('pixelSpacingInMeter', 10.0)
-params.put('nodataValueAtSea', False)
+parameters.put('demResamplingMethod', 'BILINEAR_INTERPOLATION')
+parameters.put('imgResamplingMethod', 'BILINEAR_INTERPOLATION')
+parameters.put('demName', 'ASTER 1Sec GDEM')
+parameters.put('pixelSpacingInMeter', 10.0)
+parameters.put('nodataValueAtSea', False)
 parameters.put('sourceBands', 'Sigma0' + '_' + band)
 
 terrains = []
