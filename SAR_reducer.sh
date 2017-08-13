296d0589ca6ab54f61297c3153ee85b720a17cd1
SimonNtz <simon.ntz@gmail.com>
2017-08-13 23:27:02 +0200

    typoo

--- SAR_reducer.sh
+++ SAR_reducer.sh
@@ -13,5 +13,5 @@ echo "@REDUCER_RUN - "$(timestamp): "start processing"
 output=SAR_animation_$(date +%s).gif
 base_path=~/SAR_app/deployment/reducer
 convert -delay 90 -loop 0 $base_path/*.png $base_path/$output
-echo "@REDUCER_RUN - "$(timestamp): "finish processing - \
-                    $output - get_file_size $output"
+echo "@REDUCER_RUN - "$(timestamp): "finish processing"
+#get_file_size $output"
