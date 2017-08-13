592dd4f6b3fb2baec01e792b7f09843d6ab9986b
SimonNtz <simon.ntz@gmail.com>
2017-08-13 19:39:10 +0200

    change typo in reduce

--- SAR_reducer.sh
+++ SAR_reducer.sh
@@ -9,9 +9,9 @@ set -e
 set -x
 source ~/SAR_app/deployment/lib.sh
 
-echo "@REDUCER_RUN - "$(timestamp): "start conversion"
+echo "@REDUCER_RUN - "$(timestamp): "start processing"
 output=SAR_animation_$(date +%s).gif
 base_path=~/SAR_app/deployment/reducer
 convert -delay 90 -loop 0 $base_path/*.png $base_path/$output
-echo "@REDUCER_RUN - "$(timestamp): "finish conversion - \
+echo "@REDUCER_RUN - "$(timestamp): "finish processing - \
                     $output - get_file_size $output"
